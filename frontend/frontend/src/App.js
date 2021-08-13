1b985866917f383b486e75123173007257550a6a
Rorychan <andrei@selfbook.com>
2021-08-13 12:59:49 +0300

    updated api url

--- frontend/frontend/src/App.js
+++ frontend/frontend/src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 
 function App() {
   const [value,setvalue] =  useState(undefined)
-  const request =() =>{ fetch("http://localhost:5000/").then(response => response.text()).then(res => setvalue(res))};
+  const request =() =>{ fetch("http://api:5000/").then(response => response.text()).then(res => setvalue(res))};
   
   return (
     <div className="App">
