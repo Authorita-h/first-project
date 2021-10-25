8688dac6856c5ab8422051c921b5b721c6b3db0d
m <m@n>
2021-10-25 15:27:52 +0000

    deleted dependency

--- terraform/service/main.tf
+++ terraform/service/main.tf
@@ -13,7 +13,4 @@ resource "aws_ecs_service" "service" {
   cluster         = var.ecs_cluster_id
   task_definition = var.ecs_task_defenition_arn
   desired_count   = var.task_count
-  depends_on = [
-    aws_iam_role_policy_attachment.ecs_agent
-  ]
 }
