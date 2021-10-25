8007d67513a71dcc796375d29543ea8321e558d2
m <m@n>
2021-10-25 13:57:31 +0000

    added backend for tfstate file

--- terraform/service/main.tf
+++ terraform/service/main.tf
@@ -1,5 +1,13 @@
 provider "aws" {}
 
+terraform {
+  backend "s3" {
+    bucket = "remote-state.bucket"
+    key    = "service/terraform.tfstate"
+    region = "eu-central-1"
+  }
+}
+
 resource "aws_ecs_service" "service" {
   name            = "${local.workspace}-service"
   cluster         = var.ecs_cluster_id
